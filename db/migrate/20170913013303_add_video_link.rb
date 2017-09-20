87734161a3f55f401a94df47a6e046af46220315
dickson <kr.d.tsl@gmail.com>
2017-09-19 23:38:39 -0400

    got controls to show

--- db/migrate/20170913013303_add_video_link.rb
+++ db/migrate/20170913013303_add_video_link.rb
@@ -1,6 +1,6 @@
 class AddVideoLinks < ActiveRecord::Migration[5.1]
   def change
-    create_table :video_links do |t|
+    create_table :videos do |t|
       t.string :link, null: false
       t.string :name, null: false
       t.jsonb :metadata
