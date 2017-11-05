ea053c440cc0d600ec0ea510bff0c9be0b354d93
Joseph Cho <jscho13@gmail.com>
2017-11-05 02:12:44 -0500

    update seconds to hours and minutes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 module ApplicationHelper
-
+  def formatted_duration total_seconds
+    hours = total_seconds / (60 * 60)
+    minutes = (total_seconds / 60) % 60
+    seconds = total_seconds % 60
+    "#{ hours }h #{ minutes }m"
+  end
 end
