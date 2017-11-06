54bc4550bf595d9bb633b7d7661aea8458247c01
Joseph Cho <jscho13@gmail.com>
2017-11-05 20:04:27 -0500

    redesign navbar header

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,25 +18,36 @@
 $(document).ready(function() {
   $('select').material_select();
 
-  var scroll_top_duration = 700;
-  var $back_to_top = $('.cd-top');
+  // This shit don't work!
+  // var scroll_top_duration = 700;
+  // var $back_to_top = $('.cd-top');
 
   //smooth scroll to top
-  $back_to_top.on('click', function(event){
-		event.preventDefault();
-		$('body,html').animate({
-			scrollTop: 0 ,
-		 	}, scroll_top_duration
-		);
-	});
+  // $back_to_top.on('click', function(event){
+	// 	event.preventDefault();
+	// 	$('body,html').animate({
+	// 		scrollTop: 0 ,
+	// 	 	}, scroll_top_duration
+	// 	);
+	// });
 
   $("#search-submit").on("click", function(e) {
     e.preventDefault();
-    var searchModelForm = $('#search-models');
-    var emptyInputFields = $('#search-models input:text').filter(function() { return $(this).val() == ""; });
+    var form1 = $('#index-search-input');
+    var form2 = $('#nav-search-input');
+    var searchModelForm = {
+      combine: function (form1, form2) {
+        $('#' + form1 + ' :input[isacopy]').remove();
+        $('#' + form2 + ' :input').not(':submit').not('textarea').not('select').each(function() { $(this).attr('value', $(this).val()); });
+        $('#' + form2 + ' textarea').each(function() { $(this).text($(this).val()); });
+        $('#' + form2 + ' select').each(function() { $('option[value!="' + $(this).val() + '"]', this).remove(); $('option[value="' + $(this).val() + '"]', this).attr('selected', 'selected'); });
+        $('#' + form2 + ' :input').not(':submit').clone().hide().attr('isacopy','y').appendTo('#' + form1);
+        return true;
+      }
+    };
+    var emptyInputFields = $('.search-models input:text').filter(function() { return $(this).val() == ""; });
     emptyInputFields.attr('disabled', true);
+
     searchModelForm.submit();
   });
-
-  
 });
