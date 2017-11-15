cbd176c4a41994f69c41bec18d17fa1cad3cb580
Joseph Cho <jscho13@gmail.com>
2017-11-14 23:00:22 -0500

    get double form submission working and rename base to custom

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,14 +15,25 @@
 //= require jquery_ujs
 //= require_tree .
 
+function submitSearch() {
+  event.preventDefault();
+  $.each ( $("#nav-search-input input, #index-search-input input").serializeArray(), function ( i, obj ) {
+    $('<input type="hidden">').prop( obj ).appendTo( $('#hidden-form') );
+  } );
+  var emptyInputFields = $('form input:hidden').filter(function() { return $(this).val() == ""; });
+  emptyInputFields.attr('disabled', true);
+  
+  $("#hidden-form").submit();
+}
+
 $(document).ready(function() {
   $('select').material_select();
 
   // This shit don't work!
   // var scroll_top_duration = 700;
   // var $back_to_top = $('.cd-top');
-
-  //smooth scroll to top
+  // 
+  // Smooth scroll to top
   // $back_to_top.on('click', function(event){
 	// 	event.preventDefault();
 	// 	$('body,html').animate({
@@ -30,24 +41,4 @@ $(document).ready(function() {
 	// 	 	}, scroll_top_duration
 	// 	);
 	// });
-
-  $("#search-submit").on("click", function(e) {
-    e.preventDefault();
-    var form1 = $('#index-search-input');
-    var form2 = $('#nav-search-input');
-    var searchModelForm = {
-      combine: function (form1, form2) {
-        $('#' + form1 + ' :input[isacopy]').remove();
-        $('#' + form2 + ' :input').not(':submit').not('textarea').not('select').each(function() { $(this).attr('value', $(this).val()); });
-        $('#' + form2 + ' textarea').each(function() { $(this).text($(this).val()); });
-        $('#' + form2 + ' select').each(function() { $('option[value!="' + $(this).val() + '"]', this).remove(); $('option[value="' + $(this).val() + '"]', this).attr('selected', 'selected'); });
-        $('#' + form2 + ' :input').not(':submit').clone().hide().attr('isacopy','y').appendTo('#' + form1);
-        return true;
-      }
-    };
-    var emptyInputFields = $('.search-models input:text').filter(function() { return $(this).val() == ""; });
-    emptyInputFields.attr('disabled', true);
-
-    searchModelForm.submit();
-  });
 });
